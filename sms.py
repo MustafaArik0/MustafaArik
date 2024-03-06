bdccff497412e06b029f70430c78d3d0ad06db3d
MustafaArik0 <128095910+MustafaArik0@users.noreply.github.com>
2024-03-06 22:05:07 +0300

    Add files via upload

---
+++ sms.py
@@ -0,0 +1,32 @@
+import requests
+from random import choice
+from string import ascii_lowercase
+from colorama import Fore, Style
+
+
+class SendSms():
+    adet = 0
+    
+    def __init__(self, phone, mail):
+        self.phone = str(phone)
+        if len(mail) != 0:
+            self.mail = mail
+        else:
+            self.mail = ''.join(choice(ascii_lowercase) for i in range(20))+"@gmail.com"
+
+
+    #Instagram
+    def Instagram(self):    
+        try:    
+            url = "https://core.instagram.com:443/api/users/sms/send"
+            headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:121.0) Gecko/20100101 Firefox/121.0", "Accept": "application/json, text/plain, */*", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate, br", "Page-Url": "/kayit-ol", "Content-Type": "application/json;charset=utf-8", "Positive-Client": "instagram", "Positive-Client-Type": "web", "Store-Id": "1", "Origin": "https://www.instagram.com", "Dnt": "1", "Sec-Gpc": "1", "Referer": "https://www.instagram.com/", "Sec-Fetch-Dest": "empty", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-site", "Te": "trailers", "Connection": "close"}
+            json={"mobile_number": self.phone, "token_type": "register_token"}
+            r = requests.post(url, headers=headers, json=json, timeout=6)
+            if r.status_code == 200:
+                print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> core.instagram.com")
+                self.adet += 1
+            else:
+                raise
+        except:    
+            print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> core.instagram.com")
+        
